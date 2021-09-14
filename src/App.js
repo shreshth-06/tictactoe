41bfc1390a0934afa53e0ec6971b58d7841697dc
shreshth-06 <shreshthchaturvedi2401@gmail.com>
2021-09-14 16:03:00 +0530

    displayed draw message with conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 /* eslint import/newline-after-import: "off" */
 import Board from './components/Board';
 import History from './components/History';
+import StatusMessage from './components/StatusMessage';
 import { calculateWinner } from './helpers';
 
 import './styles/root.scss';
@@ -15,9 +16,7 @@ const App = () => {
   const current = history[currentMove];
 
   const winner = calculateWinner(current.board);
-  const message = winner
-    ? `Winner is ${winner}`
-    : `Next player is ${current.isXNext ? 'X' : '0'}`;
+
   const handleSquareClick = position => {
     if (current.board[position] || winner) {
       return;
@@ -46,7 +45,7 @@ const App = () => {
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <h1>{message}</h1>
+      <StatusMessage winner={winner} current={current} />
       <Board board={current.board} handleSquareClick={handleSquareClick} />
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
